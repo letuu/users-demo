618db371470fa744ad2b8d9aa95f2ca971a04091
letuu <albertvt@ya.ru>
2023-01-12 04:29:55 +0300

    Initial commit

---
+++ src/test/java/pro/sky/users/UsersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
